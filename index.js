addf1862a2a5943464e2e3a7c78aa8e135889395
myFirstgitrepo <tanvirmahin607@gmail.com>
2024-12-23 19:37:03 +0600

    added a post method for all blogs

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 require('dotenv').config()
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
+//Blogs_web
+//1qsvl66qqjgIJwY4
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kriop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        
+        // blog related apis
+        const blogsCollection = client.db('Blogs_Web').collection('Blogs');
+
+        app.post('/blogs', async (req, res) => {
+            const blog = req.body;
+            console.log(blog);
+            const result = await blogsCollection.insertOne(blog);
+            res.send(result);
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('server is running');
 })
 
