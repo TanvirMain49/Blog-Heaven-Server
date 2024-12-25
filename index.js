cf2483995c6c706c03b129f7c815c1e017bf1315
myFirstgitrepo <tanvirmahin607@gmail.com>
2024-12-25 19:53:47 +0600

    added jwt token

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ require('dotenv').config()
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 4000;
 
+const corsOptional = {
+    origin: ['http://localhost:5173'],
+    credentials: true
+}
 const app = express();
-app.use(cors());
+app.use(cors(corsOptional));
 app.use(express.json());
+app.use(cookieParser());
 
-//Blogs_web
-//1qsvl66qqjgIJwY4
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
 
+    if (!token) {
+        return res.status(401).send({ message: "unauthorize access (not valid token)" });
+    }
+    jwt.verify(token, (process.env.JWT_TOKEN), (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unauthorize access" });
+        }
+        req.user = decoded; //this the user token 
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kriop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,6 +49,29 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+        //jwt webToken
+        app.post('/jwt', async (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, (process.env.JWT_TOKEN), { expiresIn: '365d' })
+            console.log(token);
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                })
+                .send({ status: true });
+        })
+
+        app.get('/logout', async (req, res) => {
+            res.clearCookie('token', {
+                maxAge: 0,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            })
+                .send({ status: true });
+        })
+
 
         // all api collections
         const blogsCollection = client.db('Blogs_Web').collection('Blogs');
@@ -41,7 +82,12 @@ async function run() {
             const result = await blogsCollection.find().toArray();
             res.send(result);
         })
-        app.get('/all-blogs/:id', async (req, res) => {
+        app.get('/all-blogs-table', async (req, res) => {
+            const result = await blogsCollection.find().limit(10).toArray();
+            res.send(result);
+        })
+
+        app.get('/all-blogs/:id',  async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await blogsCollection.findOne(query);
@@ -52,23 +98,23 @@ async function run() {
             const result = await blogsCollection.insertOne(blog);
             res.send(result);
         })
-        app.put('/update-blogs/:id', async(req, res)=>{
+        app.put('/update-blogs/:id',  async (req, res) => {
             const id = req.params.id;
-            const updateBlog = req.body; 
-            const filter = {_id : new ObjectId(id)};
+            const updateBlog = req.body;
+            const filter = { _id: new ObjectId(id) };
             const update = {
                 $set: updateBlog
             }
-            const option = {upsert: true};
+            const option = { upsert: true };
             const result = await blogsCollection.updateOne(filter, update, option);
             res.send(result);
         })
 
 
-//--------------------------------------For Comment section api---------------------------------
-        app.get('/blog-Comment/:id', async (req, res) => {
+        //--------------------------------------For Comment section api---------------------------------
+        app.get('/blog-Comment/:id',  async (req, res) => {
             const commentId = req.params.id;
-            const filter = {commentId: commentId}
+            const filter = { commentId: commentId }
             const result = await commentsCollection.find(filter).toArray();
             // console.log(result);
             res.send(result);
@@ -79,30 +125,36 @@ async function run() {
             const result = await commentsCollection.insertOne(comment);
             res.send(result);
         })
-// -------------------------------------------WishList----------------------------------------
+        // -------------------------------------------WishList----------------------------------------
 
-        app.post('/wishList', async(req, res)=>{
+        app.post('/wishList',  async (req, res) => {
             const wishList = req.body;
-            const query = {userEmail: wishList.userEmail, blog_id: wishList.blog_id}
+            const query = { userEmail: wishList.userEmail, blog_id: wishList.blog_id }
             console.log(query);
             const alreadyExit = await wishListCollection.findOne(query);
-            if(alreadyExit){
+            if (alreadyExit) {
                 return res.status(400).send('Already Exist');
             }
             const result = await wishListCollection.insertOne(wishList);
             res.send(result);
         })
 
-        app.get('/wishList/:email', async(req, res)=>{
+        app.get('/wishList/:email', verifyToken, async (req, res) => {
+            const decodedEmail = req.user?.email;
             const email = req.params.email;
-            const query = {userEmail: email}
+            console.log('decodedEmail: ', decodedEmail);
+            console.log('Email: ', email);
+            if(decodedEmail != email){
+                return res.status(401).send({ message: "unauthorize access" });
+            }
+            const query = { userEmail: email }
             const result = await wishListCollection.find(query).toArray()
             res.send(result);
         })
 
-        app.delete('/wishList/:id', async(req, res)=>{
+        app.delete('/wishList/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await wishListCollection.deleteOne(query);
             res.send(result);
         })
